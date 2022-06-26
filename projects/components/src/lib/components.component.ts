0ef615b013dc7e1d64fb1266e8ca667c8e493a13
Kirill Zdornyy <kirill@getfillet.com>
2022-06-26 15:29:26 -0400

    Allow currency code override.

--- projects/components/src/lib/components.component.ts
+++ projects/components/src/lib/components.component.ts
@@ -22,16 +22,28 @@ export class ComponentsComponent implements OnInit {
     return this._username;
   }
 
-  set username(message: string) {
-    this._username = message;
+  set username(s: string) {
+    this._username = s;
   }
 
   private _username = '';
 
-  constructor(public http: HttpClient) {
+  @Input()
+  // Must initialize because `strictPropertyInitialization` is enabled in tsconfig.json.
+  get currency(): string {
+    return this._currency;
   }
 
+  set currency(s: string) {
+    this._currency = s;
+  }
+
+  private _currency = '';
+
   menuItems?: Observable<MenuItem[]>;
+
+  constructor(public http: HttpClient) {
+  }
 
   ngOnInit(): void {
     this.menuItems = this.http.get<MenuItem[]>(`${this.apiUrlRoot}/${this.username}`);
--- projects/elements/src/app/app.module.ts
+++ projects/elements/src/app/app.module.ts
@@ -1,8 +1,21 @@
-import {Injector, NgModule} from '@angular/core';
+import {DEFAULT_CURRENCY_CODE, Injector, LOCALE_ID, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {ComponentsModule, ComponentsComponent} from 'components';
 import {createCustomElement} from "@angular/elements";
 import {HttpClientModule} from "@angular/common/http";
+import {getLocaleCurrencyCode} from "@angular/common";
+
+/**
+ * USD currency code that the application uses by default for CurrencyPipe when no
+ * DEFAULT_CURRENCY_CODE is provided. [1]
+ *
+ * [1] packages/core/src/i18n/localization.ts
+ */
+export const USD_CURRENCY_CODE = 'USD';
+
+function getDefaultCurrencyCode(locale: any): string {
+  return getLocaleCurrencyCode(locale) ?? USD_CURRENCY_CODE;
+}
 
 @NgModule({
   declarations: [],
@@ -12,7 +25,10 @@ import {HttpClientModule} from "@angular/common/http";
     HttpClientModule,
     ComponentsModule,
   ],
-  providers: [],
+  providers: [
+    // https://github.com/angular/angular/pull/34724#issuecomment-614496201
+    {provide: DEFAULT_CURRENCY_CODE, useFactory: getDefaultCurrencyCode, deps: [LOCALE_ID]},
+  ],
   bootstrap: []
 })
 export class AppModule {
